73ce6910dababc4c7215a48681480dee785854e9
gadz <gadz03080@gmail.com>
2026-08-26 17:18:01 +0500

    Update run_translator.bat

--- run_translator.bat
+++ run_translator.bat
@@ -1,6 +1,6 @@
 <<<<<<< HEAD
 @echo off
-echo Запуск переводчика и Qwen (лезгинский ↔ русский)...
+echo Запуск  (лезгинский ↔ русский)...
 cd /d "%~dp0"
 call .venv\Scripts\activate.bat
 python translate.py
